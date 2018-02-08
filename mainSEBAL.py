922a294202bfbd8ad085327d0e6773f31f26f2c9
ardian923 <ardi.plj@gmail.com>
2018-02-08 13:50:30 +0700

    Update mainSEBAL.py

--- mainSEBAL.py
+++ mainSEBAL.py
@@ -16,4 +16,33 @@ def toaAlbedo():
     return
 
 
+def reflectivity():
+    
+    return
+
+def spectralRadiance():
+    
+    return
+
+def surfaceEmissivity_NB():
+    
+    return
+
+def surfaceEmissivity_o():
+    
+    return
+
+# NDVI
+# SAVI
+# LAI
+# Surface Temperature
+
+def outgoingLongwave():
+    
+    return
+
+
+
+
+
    
